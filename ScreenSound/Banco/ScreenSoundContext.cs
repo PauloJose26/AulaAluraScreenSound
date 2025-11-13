3d907f37dbd764e237ba36435043c218e0301c73
PauloJose26 <paulojoliveira.2616@gmail.com>
2025-11-13 15:51:18 -0300

    Adição das Migrations no projeto e alteração no banco de dados

--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -5,7 +5,7 @@ namespace ScreenSound.Banco;
 
 internal class ScreenSoundContext: DbContext
 {
-    private readonly string stringConnection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    private readonly string stringConnection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
     public DbSet<Artista> Artistas { get; set; }
     public DbSet<Musica> Musicas { get; set;  }
